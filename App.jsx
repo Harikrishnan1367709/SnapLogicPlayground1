b5f82e247606b70dc2b1e97169abe804da19c612
Harikrishnan1367709 <harikrishnan@mulecraft.in>
2025-01-06 19:16:33 +0530

    created a sample playground ui

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import { DataTransformPlayground } from './components/ui/DataTransformPlayground'
+import { ThemeProvider } from './components/ui/theme-provider'
+
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">SnapLogic Data Transform Playground</h1>
+        <DataTransformPlayground />
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
+
